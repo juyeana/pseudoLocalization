b2ee9e0866e7abe3b4785ff59a9b9bc00fcb7833
Juyean Lee <juyeana@naver.com>
2021-02-20 04:50:19 -0800

    initial react

--- route/api/pseudo.js
+++ route/api/pseudo.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const fs = require("fs");
-const hash = require("hash.js");
+const express = require('express');
+const fs = require('fs');
+const hash = require('hash.js');
 const chars = JSON.parse(
   fs.readFileSync(`${__dirname}/../../data/charMap.json`)
-)
+);
 
 const router = express.Router();
 
@@ -12,45 +12,42 @@ const router = express.Router();
 // @access PUBLIC
 
 router.post('/', async (req, res, next) => {
+  const { inputStr, wrapperChecked, idChecked } = await req.body;
 
+  let output = '';
 
-    const {inputStr, checked} = await req.body;
-    
-    let output=""
-    
-    // prepend and append string to wrap altered string
-    const prepend='[||'
-    const append='||]'
+  // prepend and append string to wrap altered string
+  const prepend = '[||';
+  const append = '||]';
 
-
-    if(inputStr){
-    
-      // iterate the input string
-      // letters are altered to a pseudo characters 
-      // empty string and special characters remained the same
-      for(const letter of inputStr){
-        
-        output = chars[letter]? output.concat(chars[letter]) : output.concat(letter)
-        
-      }
-      // only add wrapper characters when it was checked
-      if(checked){
-      output = prepend+output+append
+  if (inputStr) {
+    // iterate the input string
+    // letters are altered to a pseudo characters
+    // empty string and special characters remained the same
+    for (const letter of inputStr) {
+      output = chars[letter]
+        ? output.concat(chars[letter])
+        : output.concat(letter);
+    }
+    // only add wrapper characters when it was checked
+    if (wrapperChecked) {
+      output = prepend + output + append;
     }
 
     // generate hash of output result
-    let hashGenerated = hash.sha256().update(output).digest('hex')
+    let hashGenerated = hash.sha256().update(output).digest('hex');
 
     // generate hash id based on the generated hash
-    let hashId = (parseInt(hashGenerated, 16) % 10**5)
+    let hashId = parseInt(hashGenerated, 16) % 10 ** 5;
 
     console.log(hashGenerated, hashId);
-    output = `${hashId}_${output}`
-    res.status(200).json(output)
-  }else{
-    res.status(400).json('No input is provided')
+    if (idChecked) {
+      output = `${hashId}_${output}`;
+    }
+    res.status(200).json(output);
+  } else {
+    res.status(400).json('No input is provided');
   }
-})
+});
 
-
-module.exports = router
+module.exports = router;
